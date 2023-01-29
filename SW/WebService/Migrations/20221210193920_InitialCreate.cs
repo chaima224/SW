a2d2165ceb263ad80a301d602102f7bd9bf891a1
Bouzid Chaima <chaima1.bouzid@gmail.com>
2023-01-29 20:42:42 +0100

    first commit

---
+++ SW/WebService/Migrations/20221210193920_InitialCreate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebService.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Congés",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateDebut = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateFin = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Verifie = table.Column<bool>(type: "bit", nullable: false),
+                    type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AjoutPar = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Congés", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Utilisateurs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MotDePasse = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Prénom = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumb = table.Column<int>(type: "int", nullable: false),
+                    SoldeCongé = table.Column<int>(type: "int", nullable: false),
+                    TempsTravail = table.Column<int>(type: "int", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Utilisateurs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Congés");
+
+            migrationBuilder.DropTable(
+                name: "Utilisateurs");
+        }
+    }
+}
